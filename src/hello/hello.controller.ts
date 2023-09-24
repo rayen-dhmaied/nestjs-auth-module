333d0fb311800751b9d4540691f44e28f859853c
rayen-dhmaied <dhmaied5@gmail.com>
2023-09-24 11:59:45 +0100

    Setup the hello module for testing

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Public } from 'src/auth/decorator/public.decorator';
 
 @Controller('hello')
-export class HelloController {}
+export class HelloController {
+
+    //Protected route because the authentification is enabled globally!
+    @Get("")
+    helloWorld(){
+        return "Hello World! User is authentified!"
+    }
+
+    //Public route
+    @Get("public")
+    @Public()
+    helloWorldPublic(){
+        return "Hello World! no authentification is required!"
+    }
+}
