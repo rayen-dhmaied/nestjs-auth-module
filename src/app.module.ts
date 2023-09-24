52a077e946c4393231d15b7b272174a0f66c4f27
rayen-dhmaied <dhmaied5@gmail.com>
2023-09-24 10:26:15 +0100

    Initialized app modules and installed necessary packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { HelloModule } from './hello/hello.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, HelloModule],
 })
 export class AppModule {}
