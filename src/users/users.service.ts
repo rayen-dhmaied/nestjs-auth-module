aa8afff9c2afb58b7fe07175943333fc8eadb708
rayen-dhmaied <dhmaied5@gmail.com>
2023-09-24 10:35:16 +0100

    Setup users module for fetching  users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+    userId : number,
+    email : string,
+    password : string
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users = [
+        {
+            userId: 1,
+            email: "test@gmail.com",
+            password: 'password123',
+        },
+        {
+            userId: 2,
+            email: 'test2@gmail.com',
+            password: 'password2021',
+        },
+        {
+            userId: 3,
+            email: 'fafa@gmail.com',
+            password: '12345678',
+        },
+    ];
+
+    async findOne(email: string): Promise<User | undefined> {
+        return this.users.find(user => user.email === email);
+    }
+}
